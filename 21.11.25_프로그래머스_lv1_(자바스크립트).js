5766ba0d25a97b9d8a90693ce7fb834b181e7d1b
YUNHO KANG <ansghkdbsgh@gmail.com>
2021-11-25 22:46:53 +0900

    type : Sort time : 15 min body : array.sort()는 문자열을 기준으로 한다.             array.sort((a,b)=>a-b) 해줘야 number 기준임.

---
+++ 21.11.25_프로그래머스_lv1_(자바스크립트).js
@@ -0,0 +1,12 @@
+function solution(array, commands) {
+    var answer = [];
+    for (let a of commands){
+        let i = a[0];
+        let j = a[1];
+        let k = a[2];
+        let temp = array.slice(i-1,j);
+        temp = temp.sort((a,b)=>a-b); //문자열을 sort함 javascript는
+        answer.push(temp[k-1]);
+    }
+    return answer;
+}
